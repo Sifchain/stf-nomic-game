8a51096641ac1198441a74d3c0323301bb85f666
Mustafa Küçük <kucukmustafa468@gmail.com>
2024-04-04 18:17:40 +0300

    feat: Integrate Flask-WTF, Flask-Limiter, and Flask-CORS app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+CSRFProtect(app)
+limiter = Limiter(app, key_func=get_remote_address)
+CORS(app)
 
 @app.route('/')
 def hello_world():
