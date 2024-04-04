e5e7e94b3858e1ea1198d99d38c6fb19908ecbc6
Mustafa Küçük <kucukmustafa468@gmail.com>
2024-04-04 18:16:52 +0300

    feat: Add new game routes app.py

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Welcome to our Flask app!'
+@app.route('/start_game')
+def start_game():
+    return 'Starting a new game!'
 
+@app.route('/join_game')
+def join_game():
+    return 'Joining a game!'
+
+@app.route('/take_turn')
+def take_turn():
+    return 'Taking a turn in the game!'
 if __name__ == '__main__':
     app.run(debug=True)
