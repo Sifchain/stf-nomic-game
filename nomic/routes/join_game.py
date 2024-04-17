a0bbe01fa188067382e40fa77ac5587dc774bde2
Snobbish Bee <125891987+snobbee@users.noreply.github.com>
2024-04-17 07:42:58 +0200

    fix: endpoint responses

--- nomic/routes/join_game.py
+++ nomic/routes/join_game.py
@@ -2,6 +2,7 @@ import json
 
 from fastapi import APIRouter, Depends
 
+from nomic.database.models.user import User
 from nomic.routes.ws import broadcast_message
 from nomic.utils.jwt_handler import get_current_user
 
@@ -9,19 +10,23 @@ router = APIRouter()
 
 
 @router.post("/join-game/{game_id}")
-async def join_game(game_id: str, current_user: str = Depends(get_current_user)):
+async def join_game(game_id: str, current_user: User = Depends(get_current_user)):
     # Check if the game exists and add the player to the game
 
     join_details = {
         "message": "Joined the game successfully",
         "game_id": game_id,
-        "current_user": current_user,
+        "user_id": str(current_user.id),
     }
 
     # Broadcast the player joining the game to all WebSocket clients
     await broadcast_message(
         json.dumps(
-            {"event_type": "join_game", "game_id": game_id, "username": current_user}
+            {
+                "event_type": "join_game",
+                "game_id": game_id,
+                "user_id": str(current_user.id),
+            }
         )
     )
 
