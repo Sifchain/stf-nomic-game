66320a964e132ddd0a512b57175fa6c12c39fcf9
Snobbish Bee <125891987+snobbee@users.noreply.github.com>
2024-04-19 23:38:20 +0200

    ci: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Reset the database
+psql -U postgres -d postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='nomic';"
+psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS nomic;"
+psql -U postgres -d postgres -c "CREATE DATABASE nomic;"
+
 # Run Alembic migrations
 echo "Running Alembic migrations..."
 python ./migration.py
